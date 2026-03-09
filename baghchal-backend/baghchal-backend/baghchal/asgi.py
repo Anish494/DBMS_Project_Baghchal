e72341651c8dc78c695847b52a8a093bafac0837
Anish Kumar Singh <anish494@gmail.com>
2026-03-09 19:17:06 +0545

    fix: remove AllowedHostsOriginValidator to allow WSS from Vercel

--- baghchal-backend/baghchal-backend/baghchal/asgi.py
+++ baghchal-backend/baghchal-backend/baghchal/asgi.py
@@ -1,7 +1,6 @@
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'baghchal.settings')
@@ -11,7 +10,5 @@ from core.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        URLRouter(websocket_urlpatterns)
-    ),
+    "websocket": URLRouter(websocket_urlpatterns),
 })
